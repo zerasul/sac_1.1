2945fd5bcbdc71b78c5463ae8d270363608cea64
mr.caver <mr.cavern@gmail.com>
2014-03-16 11:45:31 +0100

    Added Languages.h containing the text strings in english & spanish

---
+++ SAC_1.1/languages.h
@@ -0,0 +1,61 @@
+/*
+ * languages.h
+ *
+ *  Created on: 16/03/2014
+ *      Author: dcuevas
+ */
+
+#define MAX_LANGUAGE 4
+typedef struct TranslatedString {
+  const char *languages[MAX_LANGUAGE];
+} TranslatedString;
+
+TranslatedString string_db[]={
+  /* for languages missing translations, english will be used instead */
+  {{"","",""}},
+  {{"  F.C."",C.C."}},
+  {{"E ","E "}},
+  {{"SOIL","HSO:"}},
+  {{"H ","H "," "}},
+  {{"L ","L ","L"}},
+  {{"A ","A ","A"}},
+  {{"PU ","RIEGO"}},
+  {{"LANGUAGE","IDIOMA"}},
+  {{"ENGLISH",         "ESPANOL"}},
+  {{"<enabled>",       "<activar>"}},
+  {{"<disabled>",      "<inhabilitado>"}},
+  {{"TIME",            "HORA"}},
+  {{"SOIL MOISTURE",   "HUMEDAD SUELO"}},
+  {{"CALIBRATE SAT.","CALIBRACION SAT."}},
+  {{"OFF",             "APAGADO"}},
+  {{"WATERING",      "RIEGO"}},
+  {{"HEATING",         "CALEFACCION",}},
+  {{"COOLING",         "REFRIGERACION"}},
+  {{"LIGHT",           "ILUMINACION"}},
+  {{"AIR EXTRACTION",  "EXTRAC.HUMEDAD"}},
+  {{"HUMIDIFIER",      "HUMIDIFICACION"}},
+  {{"alarm",           "Alarm"}},
+  {{"OUTPUT 1",        "SALIDA 1"}},
+  {{"OUTPUT 2",        "SALIDA 2"}},
+  {{"OUTPUT 3",        "SALIDA 3"}},
+  {{"AIR HUMIDITY",    "HUMEDAD AIRE"}},
+  {{"TEMPERATURE",     "TEMPERATURA"}},
+
+  {{"WATERING",      "RIEGO"}},
+  {{"CICLES (sec):",  "CICLOS (seg):"}},
+
+  {{"RETURN TO",       "VOLVER"}},
+  {{"MAIN MENU",       "MENU PRINCIPAL"}},
+  {{"Setup...",        "Configuracion.."}},
+  {{"OUTPUTS",          "SALIDAS"}},
+  {{"Reset",           "RESET"}},
+  {{"ON",              "ENCENDIDO"}},
+  {{"START",           "COMIENZA"}},
+  {{"DURATION",        "DURACION"}},
+  {{"CONFIGURATION",   "CONFIGURACION"}},
+  {{"Hysteresis: ",  }},
+  {{"Log"}},
+  {{"MIN:"}},  {{"MIN:"}},
+};
+
+int active_language = 0;
