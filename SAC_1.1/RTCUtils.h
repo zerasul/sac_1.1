632755b5041c98391f660a9fcdb513e96c557a85
mr.caver <mr.cavern@gmail.com>
2014-03-16 17:55:53 +0100

    Created RTCUtils with functions for time readings

---
+++ SAC_1.1/RTCUtils.h
@@ -0,0 +1,41 @@
+/*
+ * RTCUtils.h
+ *
+ *  Created on: 16/03/2014
+ *      Author: dcuevas
+ */
+
+#include "Arduino.h"
+#include "DS1307RTC.h"
+#include "Time.h"
+#include "Wire.h"
+tmElements_t tm;
+
+
+int RTCread(tmElements_t tm){
+	if (RTC.chipPresent()){
+	return RTC.read(tm);
+	}
+	else{
+		return 0;
+	}
+}
+int getHour(tmElements_t tm){
+	return tm.Hour;
+}
+int getMinutes(tmElements_t tm){
+	return tm.Minute;
+}
+int getSeconds(tmElements_t tm){
+	return tm.Second;
+}
+int getDay(tmElements_t tm){
+	return tm.Day;
+}
+int getMonth(tmElements_t tm){
+	return tm.Month;
+}
+int getYear(tmElements_t tm){
+	return tm.Year;
+}
+
