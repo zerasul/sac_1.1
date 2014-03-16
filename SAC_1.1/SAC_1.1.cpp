c75b2c4f814c956fcc109579707f60da536f959c
Victor <suarez.garcia.victor@gmail.com>
2014-03-16 11:11:46 +0100

    Initial Version

---
+++ SAC_1.1/SAC_1.1.cpp
@@ -0,0 +1,22 @@
+/* SAC_1.1: New Version */
+#include "SAC_1.1.h"
+
+#define RELAY_1_PIN 5
+#define RELAY_2_PIN 6
+#define RELAY_3_PIN 4
+
+#define BUTTON_UP_PIN 9
+#define BUTTON_ENTER_PIN 8
+#define BUTTON_DOWN_PIN 7
+
+//The setup function is called once at startup of the sketch
+void setup()
+{
+// Add your initialization code here
+}
+
+// The loop function is called in an endless loop
+void loop()
+{
+//Add your repeated code here
+}
