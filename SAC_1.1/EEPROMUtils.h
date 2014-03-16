20c4a86b13783e729a9a217f1de1914d486c3b59
Victor <suarez.garcia.victor@gmail.com>
2014-03-16 12:35:54 +0100

    Comments Added

--- SAC_1.1/EEPROMUtils.h
+++ SAC_1.1/EEPROMUtils.h
@@ -1,12 +1,20 @@
 /*
- * EEPROMUtils.h
+ * EEPROMUtils.h: this file contains all the EEPROM Functions for use in SAC Project.
  *
  *  Created on: 16/03/2014
- *      Author: victor
+ *      Author: victor suarez<suarez.garcia.victor@gmail.com>
+ *      Co-Author: David Cuevas Lopez
  */
 #include "EEPROM.h"
-#include <stdint.h>
+/*Write in The EEPROM MEmory.
+ *  Pos: Position in EEPROM Memory.
+ *  val: Value to Store.
+ */
 #define EEPROM_write(pos,val) EEPROM.write(pos,val)
+/**
+ * Read From EEPROM Memory.
+ * Pos: Position in EEPROM Memory.
+ */
 #define EEPROM_read(pos)      EEPROM.read(pos)
 
 
