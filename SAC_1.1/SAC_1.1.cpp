dcff339e02260a3482d8e87c00214a1c0e188a2c
mr.cavern <mr.cavern@gmail.com>
2014-03-16 11:20:31 +0100

    Initial comments from Rev 1

--- SAC_1.1/SAC_1.1.cpp
+++ SAC_1.1/SAC_1.1.cpp
@@ -1,4 +1,36 @@
 /* SAC_1.1: New Version */
+/* SAC - automated agriculture System http://sacultivo.com/
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General/ Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+Written by Øyvind Kolås pippin@gimp.org in 2013, based around a core of
+sensor initialization and reading by by Andres Orencio Ramirez Perez
+andy@orencio.org
+
+Rev 2.0
+Adapted previous code with new SerLCD Library for using 20x4 displays, with newer functions to control
+LCD. Added new Menu's and removed some unused code for clearer comprehension.
+
+In future releases some new sensors will be added in addition to a hardware clock for simplifying all time
+functions and readings.
+
+Code adapted by David Cuevas <mr.cavern@gmail.com> with the support and collaboration of Victor Suarez <zerasul@gmail.com>
+and Adrian from Artesanos Industriales del Sur.
+
+*/
+
 #include "SAC_1.1.h"
 
 #define RELAY_1_PIN 5
