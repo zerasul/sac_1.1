bf6880862b3c75f7e36206f4cf2e6d7923f6235a
mr.cavern <mr.cavern@gmail.com>
2014-03-20 16:54:44 +0100

    Added Water temperature sensor in sac_sensors

--- SAC_1.1/sac_sensors.h
+++ SAC_1.1/sac_sensors.h
@@ -7,6 +7,7 @@
  */
 
 #include "Arduino.h"
+#include "OneWire.h"
 
 // Pin for air humdity and temperature sensor (AHTS).
 #define AHTS_PIN A0
@@ -18,6 +19,13 @@
 #define MOISTURE_PIN A3
 
 #define WTS_PIN A2
+
+
+int DS18S20_Pin = A1; //DS18S20 Signal pin on Analogic 1
+
+//Temperature chip i/o
+OneWire ds(DS18S20_Pin);  // on pin A1
+
 /*------------Global Variables-------------*/
 float moisture_calib       = 500;
 int readingno=0;
@@ -133,4 +141,51 @@ void read_sensors (void)
   digitalWrite(SOIL_MOISTURE_POWER_PIN, HIGH);
   cached_moisture = analogRead(MOISTURE_PIN);
   digitalWrite(SOIL_MOISTURE_POWER_PIN, LOW);
+}
+
+float getTemp(){
+  //returns the temperature from one DS18S20 in DEG Celsius
+
+  byte data[12];
+  byte addr[8];
+
+  if ( !ds.search(addr)) {
+      //no more sensors on chain, reset search
+      ds.reset_search();
+      return -1000;
+  }
+
+  if ( OneWire::crc8( addr, 7) != addr[7]) {
+      Serial.println("CRC is not valid!");
+      return -1000;
+  }
+
+  if ( addr[0] != 0x10 && addr[0] != 0x28) {
+      Serial.print("Device is not recognized");
+      return -1000;
+  }
+
+  ds.reset();
+  ds.select(addr);
+  ds.write(0x44,1); // start conversion, with parasite power on at the end
+
+  byte present = ds.reset();
+  ds.select(addr);
+  ds.write(0xBE); // Read Scratchpad
+
+
+  for (int i = 0; i < 9; i++) { // we need 9 bytes
+    data[i] = ds.read();
+  }
+
+  ds.reset_search();
+
+  byte MSB = data[1];
+  byte LSB = data[0];
+
+  float tempRead = ((MSB << 8) | LSB); //using two's compliment
+  float TemperatureSum = tempRead / 16;
+
+  return TemperatureSum;
+
 }
